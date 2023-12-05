f749997c1d439bd55cf59a0fc929b7949cb1aad3
WilliamTatendaJose <40793621+WilliamTatendaJose@users.noreply.github.com>
2023-12-05 10:33:29 +0200

    forgot password

--- Views/ForgotPassword.xaml.cs
+++ Views/ForgotPassword.xaml.cs
@@ -6,7 +6,7 @@ public partial class ForgotPassword : ContentPage
 	public ForgotPassword()
 	{
 		
-		BindingContext = new ForgotPasswordViewModel();
+		BindingContext = new ForgotPasswordViewModel(Navigation);
         InitializeComponent();
     }
 }
